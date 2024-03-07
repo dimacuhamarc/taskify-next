858c7e57ffade7240694fcaa4b0df097bbdbb1fd
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-03-07 22:10:08 +0800

    Add category cards and fetch categories from API

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,10 +1,50 @@
+'use client'
+
+import { CategoryCard } from "@/components/cards";
 import AppTemplate from "@/templates/app-template";
+import { API_URL } from '@/utilities/services';
+
+import { UserCategories } from '@/types/app-types';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 export default function Categories() {
+  const [categories, setCategories] = useState<UserCategories[]>([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/v1/categories`, {
+          headers: {
+            'authorization': sessionStorage.getItem('token'),
+            'Accept': 'application/json',
+          }
+        });
+        setCategories(response.data);
+      }
+      catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+
   return (
     <AppTemplate>
-      <h1>Categories</h1>
+      <div className='w-full flex flex-col justify-between items-start gap-4'>
+        <h1 className='text-4xl font-bold'>Categories</h1>
+        <div className="w-full grid auto-rows-[300px] grid-cols-4 gap-6">
+          {
+            categories && categories.map((category, i) => {
+              return (
+                <CategoryCard key={i} className={`row-span-1 rounded-xl border-2 border-slate-400/10`} category={category} />
+              );
+            })
+          }
+        </div>
+      </div>
     </AppTemplate>
   );
 }
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,7 +1,20 @@
-export function Cards() {
+import { cn } from "@/utilities/tw-util";
+import { UserCategories } from "@/types/app-types";
+import formatTimeAgo from "@/utilities/date-formatter";
+
+interface CategoryCardProps {
+  className: string;
+  category: UserCategories;
+}
+
+export function CategoryCard({ className, category }: CategoryCardProps) {
   return (
-    <div>
-      <h1>Category Cards</h1>
+    <div className={cn(`btn btn-outline btn-primary animate-fade animate-once animate-ease-in flex flex-col justify-between p-6 h-auto`,className)}>
+      <div>
+        <h1 className="text-2xl font-bold">{category.title}</h1>
+        <p className="text-base opacity-35">{category.subtitle}</p>
+      </div>
+      <p className="self-end font-thin text-base">{category.created_at === category.updated_at ? 'Created ' + formatTimeAgo(category.updated_at) : 'Edited ' + formatTimeAgo(category.updated_at)}</p>
     </div>
   );
 }
