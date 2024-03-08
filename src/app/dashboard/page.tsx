f46cffd5205f9d31430ebf9ac97055988d14042d
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-03-08 18:54:26 +0800

    Add categories and tasks to Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 
 import AppTemplate from "@/templates/app-template";
-import { UserData, UserTasks } from '@/types/app-types';
+import { UserData, UserTasks, UserCategories } from '@/types/app-types';
 
 import { GetUserInfo, API_URL } from '@/utilities/services';
 
@@ -15,6 +15,49 @@ export default function Dashboard() {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [uncategorizedTasks, setUncategorizedTasks] = useState<UserTasks[]>([]);
   const [pendingTasks, setPendingTasks] = useState<UserTasks[]>([]);
+  const [tasks, setTasks] = useState<UserTasks[]>([]);
+  const [categories, setCategories] = useState<UserCategories[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/v1/categories`, {
+          headers: {
+            'authorization': sessionStorage.getItem('token'),
+            'Accept': 'application/json',
+          }
+        });
+        if (response.data) {
+          setCategories(response.data);
+          console.log('got the categories!');
+        }
+      }
+      catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/v1/tasks`, {
+          headers: {
+            'authorization': sessionStorage.getItem('token'),
+            'Accept': 'application/json',
+          }
+        });
+        if (response.data) {
+          setTasks(response.data);
+          console.log('got the tasks!');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchTasks();
+  }, []);
 
   useEffect(() => {
     const fetchPendingTasks = async () => {
@@ -71,14 +114,20 @@ export default function Dashboard() {
 
   return (
     <AppTemplate>
-      
       <div className='flex flex-row justify-between items-center'>
         <h1 className='text-4xl'>Your priorities for today!</h1>
+
         <div className='text-right'>
           <h1>Welcome to Dashboard, {userData?.name}</h1>
           <h2>{userData?.email} | UID : {userData?.id}</h2>
         </div>
       </div>
+
+      <div className='flex flex-col my-5 container bg-primary text-primary-content p-4 px-5 rounded-lg ring-0 ring-primary hover:bg-opacity-25 hover:text-primary hover:ring-1 transition-all ease-in-out'>
+        <h1>Hello, {userData?.name}</h1>
+        <h2 className='text-2xl font-bold'>Currently, You have {tasks.length > 1 ? `${tasks.length} tasks` : `${tasks.length} task`} and {categories.length > 1 ? `${categories.length} categories` : `${categories.length} category`}</h2>
+      </div>
+
       <div className='flex flex-row w-full gap-6'>
         <div className='flex flex-grow flex-col'>
           <h2 className='text-2xl font-bold'>Pending Tasks</h2>
@@ -101,6 +150,7 @@ export default function Dashboard() {
           }
         </div>
       </div>
+      
     </AppTemplate>
   );
 }
